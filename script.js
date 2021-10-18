b6438fc279824328540eaa56ccbc9f53b2f6e015
830503 <lixiaoqi19830503@gmail.com>
2021-10-18 15:21:01 +0200

    added funtion on buttons

--- script.js
+++ script.js
@@ -1,70 +1,140 @@
+const userScore = 0;
+const computerScore = 0;
+const userScoreDisplay = document.getElementById("user-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+const scoreBoard = document.querySelector(".score-board");
+
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDispaly = document.getElementById("result");
-const possibleChoices = document.querySelectorAll("img");
-const randomChoices = document.getElementById("random");
-let userChoice;
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const lizard = document.getElementById("lizard");
+const spock = document.getElementById("spock");
+const random = document.getElementById("random");
+
+/*let userChoice;
 let randomChoice;
 let computerChoice;
-let result;
+let result;*/
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors", "lizard ", "spock"];
+    const randomNumber = Math.floor(Math.random() * 5); 
+    return choices[randomNumber];
+}
+getComputerChoice();
+
+function getRandomChoice(){
+    const choices = ["rock", "paper", "scissors", "lizard ", "spock"];
+    const randomNumber = Math.floor(Math.random() * 5); 
+    return choices[randomNumber];
+}
+getRandomChoice();
 
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click",(e) => {
-    userChoice = possibleChoices.value;
-    userChoiceDisplay.innerHTML = userChoice;
-    generateComputerChoice();
-    getResult();
-}))
-
-randomChoices.addEventListener("click", (e) => {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
-    randomChoice = randomNumber;
-    userChoiceDisplay.innerHTML = randomChoice;
-    generateComputerChoice();
-})
-
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
-
-    if(randomNumber === 1){
-        computerChoice = "Rock";
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case "scissorspaper":
+        case "paperrock":
+        case "rocklizard":
+        case "lizardspock":
+        case "spockscissors":
+        case "scissorslizard":
+        case "lizardpaper":
+        case "paperspock":
+        case "spockrock":
+        case "rockscissors":
+           resultDispaly.innerHTML = "You win!";
+            break;
+        case "paperscissors":
+        case "rockpaper":
+        case "lizardrock":
+        case "spocklizard":
+        case "scissorsspock":
+        case "lizardscissors":
+        case "paperlizard":
+        case "spockpaper":
+        case "rockspock":
+        case "scissorsrock":  
+            resultDispaly.innerHTML = "You loss!";
+            break;
+        case "paperpaper":
+        case "rockrock":
+        case "lizardlizard":
+        case "spockpock":
+        case "scissorsscissors":
+            resultDispaly.innerHTML = "Its a draw!";
+            break;
     }
-    if(randomNumber === 2){
-        computerChoice = "Paper";
-    }
-    if(randomNumber === 3){
-        computerChoice = "Scissors";
-    }
-    if(randomNumber === 4){
-        computerChoice = "Lizard";
-    }
-    if(randomNumber === 5){
-        computerChoice = "Spock";
-    }   
-    computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-function getResult(){
-    if(computerChoice === userChoice){
-        result = "It's a draw! "
+function gameRandom(){
+    const computerChoice = getComputerChoice();
+    const randomChoice = getRandomChoice(); 
+    switch (randomChoice + computerChoice){
+        case "scissorspaper":
+        case "paperrock":
+        case "rocklizard":
+        case "lizardspock":
+        case "spockscissors":
+        case "scissorslizard":
+        case "lizardpaper":
+        case "paperspock":
+        case "spockrock":
+        case "rockscissors":
+           resultDispaly.innerHTML = "You win!";
+            break;
+        case "paperscissors":
+        case "rockpaper":
+        case "lizardrock":
+        case "spocklizard":
+        case "scissorsspock":
+        case "lizardscissors":
+        case "paperlizard":
+        case "spockpaper":
+        case "rockspock":
+        case "scissorsrock":  
+            resultDispaly.innerHTML = "You loss!";
+            break;
+        case "paperpaper":
+        case "rockrock":
+        case "lizardlizard":
+        case "spockpock":
+        case "scissorsscissors":
+            resultDispaly.innerHTML = "Its a draw!";
+            break;
     }
-    if(computerChoice === "rock" && userChoice === "paper"){
-        result = "You win! "
-    }
-    if(computerChoice === "rock" && userChoice === "scissors"){
-        result = "You loss! "
-    }
-    if(computerChoice === "paper" && userChoice === "scissors"){
-        result = "You win! "
-    }
-    if(computerChoice === "paper" && userChoice === "rock"){
-        result = "You loss! "
-    }
-    if(computerChoice === "scissors" && userChoice === "rock"){
-        result = "You win! "
-    }
-    if(computerChoice === "scissors" && userChoice === "paper"){
-        result = "You loss! "
-    }
-    resultDispaly.innerHTML = result;
 }
+
+function main(){
+    rock.addEventListener("click", function(){
+        game("rock");
+         
+     })
+     paper.addEventListener("click", function(){
+         game("paper");
+          
+      })
+      scissors.addEventListener("click", function(){
+         game("scissors");
+          
+      })
+      lizard.addEventListener("click", function(){
+         game("lizard");
+          
+      })
+      spock.addEventListener("click", function(){
+         game("spock");
+          
+      })
+     
+      random.addEventListener("click", function(){
+         gameRandom("");
+          
+      })
+}
+main(); 
+
